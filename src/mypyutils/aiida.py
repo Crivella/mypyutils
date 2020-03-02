0a0fa1906083b0203749f9f610824b3e11bf1071
Crivella <davidecrivella@yahoo.it>
2020-03-02 09:37:42 +0100

    WIP

--- src/mypyutils/aiida.py
+++ src/mypyutils/aiida.py
@@ -69,6 +69,7 @@ def analyze_workchain(
     node,
     report_actions={},
     # **kwargs
+    on_killed=None,
     on_running=None,
     on_finished_ok=None,
     on_failed=None,
@@ -77,27 +78,37 @@ def analyze_workchain(
     ):
     wc = validate_node(node)
 
-    if not wc.is_finished and not wc.is_excepted:
-        print(tab + 'still running')
-        if not on_running is None:
-            on_running(wc)
+    if wc.is_killed:
+        print(tab + 'Killed!!!')
+        if not on_killed is None:
+            return on_killed(wc)
+        return
+
     if wc.is_finished_ok:
         print(tab + 'finished_ok')
         if not on_finished_ok is None:
-            on_finished_ok(wc)
+            return on_finished_ok(wc)
+        return
+
     if wc.is_failed:
         print(tab + 'Failed:')
         report_failed(wc, tab=tab+'  ', actions=report_actions)
 
         if not on_failed is None:
-            on_failed(wc)
+            return on_failed(wc)
+        return
 
     if wc.is_excepted:
         print(tab + 'Excepted!!')
         report_exception(wc, tab=tab+'  ')
 
         if not on_excepted is None:
-            on_excepted(wc)
+            return on_excepted(wc)
+        return
+
+    print(tab + 'still running')
+    if not on_running is None:
+        on_running(wc)
 
 def void(*args, **kwargs):
     return
