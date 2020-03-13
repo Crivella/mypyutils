86810a5128409f283824d03a110ad7ec9aaa3b61
Crivella <davidecrivella@yahoo.it>
2020-03-13 12:25:06 +0100

    Added make_supercell function

--- src/mypyutils/aiida.py
+++ src/mypyutils/aiida.py
@@ -5,6 +5,7 @@ from scipy.spatial import KDTree
 
 from aiida import orm
 from aiida.orm.utils import load_node
+from aiida.engine import calcfunction
 from qepppy.utils import recipr_base
 
 def ListInputs_to_dict(inputs):
@@ -234,3 +235,59 @@ def analyze_FindCrossingsWorkChain(node, gap_thr=0.0025, noprint=False):
         res['distance'].append(distance)
 
     return res
+
+save = {
+    # 12523: {(2,1,1): load_node(36758)},
+    # 12155: {(2,1,1): load_node(36764)}
+    }
+def make_supercell(structure, supercell):
+    if not isinstance(supercell, orm.ArrayData):
+        app = orm.ArrayData()
+        app.set_array('data', np.array(supercell))
+        supercell = app
+    pk = structure.pk
+    sc = tuple(supercell.get_array('data'))
+    if pk in save:
+        if sc in save[pk]:
+            return save[pk][sc]
+
+    res = _make_supercell(structure, supercell)
+
+    if not pk in save:
+        save[pk] = {}
+    save[pk][sc] = res
+
+    return res
+
+@calcfunction
+def _make_supercell(structure, supercell):
+    from itertools import product
+    mag_atoms = supercell.get_array('mag_atoms')
+    sc = tuple(supercell.get_array('data'))
+    x,y,z = sc
+    grid = np.array(list(product(range(x), range(y), range(z))))
+
+    cell = structure.cell
+
+    new = orm.StructureData()
+    new.set_cell((np.array(cell).T * sc).T)
+
+    kinds = structure.kinds
+    sites = structure.sites
+
+    counter = {k.name:0 for k in kinds}
+
+    for R_cryst in grid:
+        for n,site in enumerate(sites):
+            pos = np.array(site.position) + np.dot(R_cryst, cell)
+            name = site.kind_name
+
+            counter[name] += 1
+
+            new.append_atom(
+                position=pos,
+                symbols=name,
+                name=f'{name}{counter[name] if name in mag_atoms else ""}',
+                )
+
+    return new
